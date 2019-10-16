aa7ce58a614649367b9cfd9bc77edd4921dece2a
Тимур Муталимов <mutalimov95@gmail.com>
2019-10-16 23:47:25 +0300

    fix check link

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def start(message):
     bot.reply_to(message, "Введите ссылку")
 
 
-@bot.message_handler(func=lambda url: checkers.is_url(url))
+@bot.message_handler(func=lambda msg: checkers.is_url(msg.text.encode("utf-8")))
 def link_save(message):
     chat_id = message.chat.id
     if ChatLink.select().where(chant_id=chat_id).exist():
@@ -39,7 +39,7 @@ def remove(message):
 
 
 @bot.message_handler(commands=['link'])
-def remove(message):
+def curr_link(message):
     link = ''
     try:
         obj = ChatLink.get(chat_id=message.chat.id)
