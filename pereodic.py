51620a775508baecc2da4be295f3227e18c50562
Тимур Муталимов <mutalimov95@gmail.com>
2019-10-17 01:32:57 +0300

    delimeters if have new urls

--- pereodic.py
+++ pereodic.py
@@ -15,12 +15,14 @@ def pereodic_send_links():
                 chat_id = chat['chat_id']
                 link = chat['link']
                 card_urls = parse(link)
-                bot.send_message(chat_id, '-'*5)
+                if card_urls:
+                    bot.send_message(chat_id, '-' * 5)
                 for url in card_urls:
                     if CACHE.get(format_key_for_cache(chat_id, url)) is None:
                         CACHE[format_key_for_cache(chat_id, url)] = url
                         bot.send_message(chat_id, url)
-                bot.send_message(chat_id, '-' * 5)
+                if card_urls:
+                    bot.send_message(chat_id, '-' * 5)
         except Exception as e:  # никогда не прекращаем работу
             print(e)
         time.sleep(random.randrange(60 * 5, 60 * 15))
