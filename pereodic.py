0af197a90b84db1514a527ab32b8e7451988a4d3
Тимур Муталимов <mutalimov95@gmail.com>
2019-10-17 00:07:59 +0300

    fix pereodic task

--- pereodic.py
+++ pereodic.py
@@ -19,6 +19,6 @@ def pereodic_send_links():
                     if CACHE.get(format_key_for_cache(chat_id, url)) is None:
                         CACHE[format_key_for_cache(chat_id, url)] = url
                         bot.send_message(chat_id, url)
-            time.sleep(random.randrange(60*5, 60*20))
-        except Exception:  # никогда не прекращаем работу
-            continue
+        except Exception as e:  # никогда не прекращаем работу
+            print(e)
+        time.sleep(random.randrange(60 * 5, 60 * 15))
